fc346f0fb3410bfab1003eb22d548cb20e904273
divy-sys <divy.varia.1020@gmail.com>
2023-02-09 16:57:11 +0530

    Fixed user_name to be null:true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :user_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_09_111847) do
   create_table "users", force: :cascade do |t|
-    t.string "user_name", null: false
+    t.string "user_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
