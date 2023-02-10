78f1041890c22eb2f9f4912e6fb8a0f093313364
divy-sys <divy.varia.1020@gmail.com>
2023-02-10 10:55:21 +0530

    Created controller of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   def index 
-
+    @articles = Article.all
   end
 
   def show
@@ -8,10 +9,12 @@ class ArticlesController < ApplicationController
   end
 
   def new 
-
+    @article = Article.new
   end
   def create 
-
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path
   end
   
   def edit 
@@ -19,10 +22,16 @@ class ArticlesController < ApplicationController
   end
 
   def update 
-
+    @article.update(article_params)
+    redirect_to people_path
   end
 
   def destroy 
-    
+    @article.destroy 
+    redirect_to article_path 
   end
+
+  private 
+  def find_
+  def 
 end
